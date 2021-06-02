f2594b403261c1f84d07efad896ecdcb895e0623
deventriwedi <85028481+deventriwedi@users.noreply.github.com>
2021-06-02 22:47:15 +0530

    Responsive changes

--- src/CatItems.js
+++ src/CatItems.js
@@ -5,11 +5,27 @@ const CatItems=({filterItem,CatItems})=>{
 return   <>
             
                 <div className="menu-tabs container">
-                <div className="menu-tab d-flex justify-content-around">
+                <div className="menu-tab d-flex justify-content-around width=device-width">
                 {
                     CatItems.map((CurrELem,index)=>{
-                        return  <button className="btn btn-warning" key={index} onClick={() => filterItem(CurrELem)}>{CurrELem}</button>
+                        return( 
+                        <>
+                        <div className="item1 col-12 col-md-6 col-lg-6 col-xl-4 my-5" key={index}>
+                                
 
+                                    {/* menu items description */}
+                                    <div className="col-12 col-md-12 col-lg-8">
+                                        <div className="main-title pt-4 pb-3">
+                                        <button className="btn btn-warning" key={index} onClick={() => filterItem(CurrELem)}>{CurrELem}</button>
+
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            
+                         
+</>
+                        )
                     })
                 }
                     
